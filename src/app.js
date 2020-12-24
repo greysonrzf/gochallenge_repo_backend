342c412f061b62ac03788030262284f0fa928b5f
Greyson Rampazo <greysonf@gmail.com>
2020-12-24 19:29:08 -0300

    project commit

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -11,23 +10,73 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const data = request.body
+
+  const repository = {
+    id: uuid(),
+    ...data,
+    likes: 0
+  }
+
+  repositories.push(repository)
+
+  return response.json(repository)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const id = request.params.id
+  const data = request.body
+
+  const { likes } = repositories.find(object => object.id === id)
+
+  const updatedRepoitory = {
+    id,
+    ...data,
+    likes,
+  }
+
+  repositories.forEach((repository, index) => {
+    if (repository.id === id) repositories[index] = updatedRepoitory
+  })
+
+  return response.json(updatedRepoitory)
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const id = request.params.id
+
+  repositories.forEach((repository, index) => {
+    if (repository.id === id) repositories.splice(index, 1)
+  })
+
+  return response.json({ message: "Repository deleted." })
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const id = request.params.id
+
+  const { title, url, techs, likes } = repositories.find(object => object.id === id)
+
+  const increaseLikes = likes + 1
+
+  const updatedRepoitory = {
+    id,
+    title,
+    url,
+    techs,
+    likes: increaseLikes,
+  }
+
+  repositories.forEach((repository, index) => {
+    if (repository.id === id) repositories[index] = updatedRepoitory
+  })
+
+  return response.json(updatedRepoitory)
+
 });
 
 module.exports = app;
